2103f912a65bec722074f4b2bc11377e2a070cd9
SamPy4 <sami.porio@gmail.com>
2017-09-08 17:39:50 +0300

    TCP test1

--- main_pitsakuulutus_mac.py
+++ main_pitsakuulutus_mac.py
@@ -2,22 +2,26 @@ import socket
 
 # SERVER
 
-TCP_IP = '127.0.0.1'
-TCP_PORT = 5005
+TCP_IP = "10.0.0.5"
+TCP_PORT = 12346
 BUFFER_SIZE = 20  # Normally 1024, but we want fast response
- 
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
+
+print("bind ok, waiting connection...")
 s.listen(1)
- 
+
+print("Accepting...")
 conn, addr = s.accept()
-print 'Connection address:', addr
+
+print ('Connection address:', addr)
 while 1:
     data = conn.recv(BUFFER_SIZE)
-    
+
     if not data:
         break
-    
-    print "received data:", data
+
+    print ("received data:", data)
     conn.send(data)  # echo
 conn.close()
