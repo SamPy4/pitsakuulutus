52d9020db07c664d9257a7426026203fd62c7e2f
SamPy4 <sami.porio@gmail.com>
2017-09-08 17:39:55 +0300

    TCP test1

--- main_pitsakuulutus_raspberrypi.py
+++ main_pitsakuulutus_raspberrypi.py
@@ -1,16 +1,16 @@
 import socket
 
 # CLIENT
- 
-TCP_IP = '127.0.0.1'
-TCP_PORT = 5005
+
+TCP_IP = "10.0.0.5"
+TCP_PORT = 12346
 BUFFER_SIZE = 1024
-MESSAGE = "Hello, World!"
- 
+MESSAGE = str.encode("Hello, World!")
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 s.send(MESSAGE)
 data = s.recv(BUFFER_SIZE)
 s.close()
- 
-print "received data:", data
+
+print ("received data:", data)
