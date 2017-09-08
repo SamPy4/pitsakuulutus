52ababfe9566a71f4b33fdf1ea00063c7898825e
SamPy4 <sami.porio@gmail.com>
2017-09-08 14:54:03 +0000

    PIN implementation

--- main_pitsakuulutus_raspberrypi.py
+++ main_pitsakuulutus_raspberrypi.py
@@ -1,6 +1,11 @@
 import socket
+import RPi.GPIO as GPIO
+import time
 
 # CLIENT
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(13, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
 TCP_IP = "10.0.0.5"
 TCP_PORT = 12346
@@ -9,7 +14,20 @@ MESSAGE = str.encode("Hello, World!")
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
+
+def kuulutus():
+    kuulutusBYTE = str.encode("Kuutluta!!!!")
+    s.send(kuulutusBYTE)
+    return
+
+while True:
+    if GPIO.input(13) == 1:
+       print("Button is pressed")
+       kuulutus()
+
+    time.sleep(0.5)
+
+
 data = s.recv(BUFFER_SIZE)
 s.close()
 
