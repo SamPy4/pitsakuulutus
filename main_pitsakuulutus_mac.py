911242e5fee9df0450834f30d5293b70a9b797f1
SamPy4 <sami.porio@gmail.com>
2017-09-08 18:17:05 +0300

    Logiigan hiomista

--- main_pitsakuulutus_mac.py
+++ main_pitsakuulutus_mac.py
@@ -21,17 +21,19 @@ print ('Connection address:', addr)
 def kaynnistaKuulutus():
     """ ITSE KUULUTUKSEN KÄYNNISTYS """
     print("KUULUTETAAAAN: Pitsatilauksien pitsat ovat haettavissa :)")
+    time.sleep(4)
     return
 
 while 1:
     data = conn.recv(BUFFER_SIZE)
 
-    if data[1:] == "kuulutus":
+    if data.decode() == "kuulutus":
         kaynnistaKuulutus()
+        conn.send(str.encode("valmis"))
 
     if not data:
         break
 
-    print ("received data:", data)
-    conn.send(data)  # echo
+    print ("received data:", data.decode())
+    #conn.send(data)  # echo
 conn.close()
