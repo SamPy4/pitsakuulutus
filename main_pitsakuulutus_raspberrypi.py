b383db0d0a7fb13336a64b1f74c7b937f619d1fc
SamPy4 <sami.porio@gmail.com>
2017-09-22 19:59:11 +0300

    ts

--- main_pitsakuulutus_raspberrypi.py
+++ main_pitsakuulutus_raspberrypi.py
@@ -12,6 +12,7 @@ TCP_IP = "192.168.1.38"
 TCP_PORT = 12346
 BUFFER_SIZE = 1024
 
+print("Connecting...")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 print("Connected to {}:{}".format(TCP_IP, TCP_PORT))
