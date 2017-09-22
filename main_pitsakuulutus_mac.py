75b84cfe753b764d2224ef804cebbf953f1a6bd2
SamPy4 <sami.porio@gmail.com>
2017-09-22 19:53:23 +0300

    modded

--- main_pitsakuulutus_mac.py
+++ main_pitsakuulutus_mac.py
@@ -3,69 +3,47 @@ from qlab_handler import qlab_GO
 
 # SERVER
 
-def restart_server():
-    """ Restarts the program. """
-    print("Restarting server...")
-    python = sys.executable
-    os.execl(python, python, * sys.argv)
-
-try:
-    # TCP_IP must be server IP
-    TCP_IP = "192.168.8.101"  # public IP "192.168.1.00"
-    TCP_PORT = 12345
-    BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+class server():
+    def __init__(self):
+        # TCP_IP must be server IP
+        self.TCP_IP = "192.168.1.38"  # public IP "192.168.1.00"
+        self.TCP_PORT = 12346
+        self.BUFFER_SIZE = 20  # Normally 1024, but we want fast response
 
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("Socket init")
-    s.bind((TCP_IP, TCP_PORT))
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("Socket init")
+        self.s.bind((self.TCP_IP, self.TCP_PORT))
 
-    print("Bind ok, waiting connection...")
-    s.listen(1)   # Waiting only 1 connection
+        print("Bind ok, waiting connection...")
+        self.s.listen(1)   # Waiting only 1 connection
 
-    print("Accepting...")
-    conn, addr = s.accept()
+        print("Accepting...")
+        self.conn, addr = self.s.accept()
 
-    print ('Connection address:', addr)
+        print ('Connection address:', addr)
 
-    def reconnect():
-        """ Restarts the server program in hope of a new connection """
-        print("Disconnected!!!")
-        conn.close()
-        print("Server closed")
-        restart_server()  # If restarted this will restart the program
-    #     #restart_server()
-    #     try:
-    #         print(tries)
-    #         if tries == 10:
-    #             restart_server()
-    #
-    #         print("Reconnecting...")
-    #         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #
-    #         print("Connecting...")
-    #         s.connect((TCP_IP, TCP_PORT))
-    #
-    #         print ('Connection address:', addr)
-    #         time.sleep(1)
-    #     except:
-    #         print(tries)
-    #         return
-    #     print(tries)
 
-    def haeData():
-        while True:
-            try:
-                data = conn.recv(BUFFER_SIZE)
-                break
-            except: # socket.error as e:
-                print()
-                reconnect()
-                # if e.errno == e.errno.ECONNRESET:
-                #     print("No data recieved!!!")
-                # else:
-                #     raise
-        return data
+    def restart_server():
+        """ Restarts the program. """
+        print("Restarting server...")
+        python = sys.executable
+        os.execl(python, python, * sys.argv)
+
+    def reconnect(self):
+        self.s.close()
+
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("Socket init")
+        self.s.bind((self.TCP_IP, self.TCP_PORT))
+
+        print("Bind ok, waiting connection...")
+        self.s.listen(1)   # Waiting only 1 connection
+
+        print("Accepting...")
+        self.conn, addr = self.s.accept()
+
+        print ('Connection address:', addr)
 
     def kaynnistaKuulutus():
         """ ITSE KUULUTUKSEN KÄYNNISTYS """
@@ -73,35 +51,51 @@ try:
         qlab_GO()
         return
 
-    # Estää ihmisiä rämppäämästä nappia
-    spamminesto = 15 # sekuntia
-    last_time_kuulutettu = time.time() - spamminesto
+    def disconnected(self):
+        x = 0
+        while x <= 3:
+            try:
+                self.conn.recv(self.BUFFER_SIZE)
+                break
+            except:
+                x += 1
+                print("Connection request %i" % x)
+                time.sleep(3)
 
-    # The main loop starts here
-    while True:
-        data = haeData()
+        if x <= 3: disconnected = False
+        else:      disconnected = True
 
-        if data:
-            conn.send(str.encode("Server: Request recieved\n"))
+        return disconnected
 
-        if data.decode() == "kuulutus":
-            if time.time() - last_time_kuulutettu  >= spamminesto:
-                last_time_kuulutettu = time.time()
-                conn.send(str.encode("Action: Kuulutetaan\n"))
-                kaynnistaKuulutus()
-                continue
-            else:
-                conn.send(str.encode("Action: Ei voi kuuluttaa vielä\n"))
+    def run(self):
+        # Estää ihmisiä rämppäämästä nappia
+        spamminesto = 15 # sekuntia
+        last_time_kuulutettu = time.time() - spamminesto
 
-            conn.send(str.encode("Done"))
+        # The main loop starts here
+        while True:
+            if self.disconnected():
+                self.reconnect()
 
-        # if not data:
-        #     break
+            data = self.conn.recv(self.BUFFER_SIZE)
 
-        print ("received data:", data.decode())
+            if data.decode() == "kuulutus":
+                if time.time() - last_time_kuulutettu  >= spamminesto:
+                    last_time_kuulutettu = time.time()
+                    self.conn.send(str.encode("Action: Kuulutetaan\n"))
+                    kaynnistaKuulutus()
+                    continue
+                else:
+                    self.conn.send(str.encode("Action: Ei voi kuuluttaa vielä\n"))
 
-except:
-    raise
-    print("ded")
-    exit()
-    restart_server()
+                self.conn.send(str.encode("Done"))
+
+            # if not data:
+            #     break
+
+            print ("received data:", data.decode())
+
+
+if __name__ == "__main__":
+    s = server()
+    s.run()
