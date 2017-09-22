42b680c3477baa8d224a5aeb6735738d1d46b943
SamPy4 <sami.porio@gmail.com>
2017-09-22 19:53:27 +0300

    modded

--- main_pitsakuulutus_raspberrypi.py
+++ main_pitsakuulutus_raspberrypi.py
@@ -4,76 +4,71 @@ import time, sys, os
 
 # CLIENT
 
+BUTTON = 13
 #GPIO.setmode(GPIO.BCM)
-#GPIO.setup(13, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-try:
-    TCP_IP = "192.168.8.101"
-    TCP_PORT = 12345
-    BUFFER_SIZE = 1024
-    MESSAGE = str.encode("Hello, World!")
+#GPIO.setup(BUTTON, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+
+TCP_IP = "192.168.1.38"
+TCP_PORT = 12346
+BUFFER_SIZE = 1024
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+print("Connected to {}:{}".format(TCP_IP, TCP_PORT))
+
+def restart_client():
+    """Restarts the client. """
+    python = sys.executable
+    os.execl(python, python, * sys.argv)
+
+def reconnect():
+    s.close()
+
+    sleep(10)
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((TCP_IP, TCP_PORT))
     print("Connected to {}:{}".format(TCP_IP, TCP_PORT))
-    def restart_client():
-        """Restarts the client. """
-        python = sys.executable
-        os.execl(python, python, * sys.argv)
 
-    def reconnect():
-        """ Restarts the client program in hope of a new connection """
-        print("Restarting...")
-        s.close()
-        print("Client closed")
-        print("3sec safety wait")
-        time.sleep(3) # 3sec waiting because server must be up before client
-        print("Done, now restarting")
-        restart_client()
-        # try:
-        #     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #     s.connect((TCP_IP, TCP_PORT))
-        #     print("reconnected")
-        # except:
-        #     print("reconnection failed")
-        # return
 
-    def kuulutus():
-        kuulutusBYTE = str.encode("kuulutus")
-        s.send(kuulutusBYTE)
-        return
+def kuulutus():
+    kuulutusBYTE = str.encode("kuulutus")
+    s.send(kuulutusBYTE)
+    return
 
-    def valmis():
-        """ Välkyttää ledejä tai jotain """
-        pass
-
-    while True:
-
-        # if GPIO.input(13) == 1:
-        #    print("Pyyntö lähetetty")
-        #    kuulutus()
-        cmd = ":"
-        cmd =input(">>>")
-
-        if cmd == "":
-            kuulutus()
-
+def disconnected():
+    x = 0
+    while x <= 3:
+        print("Disconnect try: %i" % x)
         try:
-           data = s.recv(BUFFER_SIZE)
+            s.send(str.encode("connection"))
+            x = 3
+            break
         except:
-           print("Disconnected")
-           reconnect()
+            x += 1
+            print("Connection request %i" % x)
+            time.sleep(3)
 
-        print("\n", data.decode(), "\n")
+    if x <= 3: disconnected = False
+    else:      disconnected = True
 
-        if data.decode() == "valmis":
-           valmis()
-
-        time.sleep(0.1)   # Estää overflown, päivittää clientiä .1sec välein
-
+    return disconnected
 
 
-    print ("received data:", data)
+def valmis():
+    """ Välkyttää ledejä tai jotain """
+    pass
 
-except:
-    raise
-    reconnect()
+while True:
+    if disconnected():
+        reconnect()
+
+    data = s.recv(BUFFER_SIZE)
+
+    # if GPIO.input(BUTTON) == 1:
+    #     print("Pyyntö lähetetty")
+    #     kuulutus()
+
+    print("\n", data.decode(), "\n")
+
+    time.sleep(0.1)   # Estää overflown, päivittää clientiä .1sec välein
