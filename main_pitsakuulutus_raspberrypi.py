3794c22c586fc9da933f5762fd293f5ba47f880f
SamPy4 <sami.porio@gmail.com>
2017-09-23 16:19:09 +0300

    First led

--- main_pitsakuulutus_raspberrypi.py
+++ main_pitsakuulutus_raspberrypi.py
@@ -83,9 +83,9 @@ while True:
     # print("Data sent")
 
     if time.time() - last_time_kuulutettu  >= spamminesto:
-        GPIO.output(LED1, "HIGH")
+        GPIO.output(LED1, True)
     else:
-        GPIO.output(LED1, "LOW")
+        GPIO.output(LED1, False)
 
     if GPIO.input(BUTTON) == 1:
         # print("Button is pressed!!!")
